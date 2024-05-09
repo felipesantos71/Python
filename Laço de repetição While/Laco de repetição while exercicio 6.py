0b8ab0e2913ab107083630e4da5d01fce702641c
Felipe Santos <159580121+felipesantos71@users.noreply.github.com>
2024-05-09 11:25:55 +0000

    lkl

--- Laço de repetição While/Laco de repetição while exercicio 6.py
+++ Laço de repetição While/Laco de repetição while exercicio 6.py
@@ -5,15 +5,18 @@ print("\n== Média e números inteiros ==")
 
 contador = 0
 soma: float = 0
+nota: float = 0
 
 while True:
-    numero = float(input(f"Digite um número: "))
-    soma+=numero
-    contador+=1
-    
-    if numero < 0:
-        break
+    nota = float(input("Digite um número: "))
+    if nota > 0:
+        contador+=1
+        soma+=nota
+    else:
+        if contador==0:
+            print("Digite novamente: ")
+        else:
+            break    
 
 media = soma / contador
-
 print(f"Média aritimética: {media}")
