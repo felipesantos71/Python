14d5abeba5b63b74c9a63ce7a3a54015b5d289b3
Felipe Santos <159580121+felipesantos71@users.noreply.github.com>
2024-05-15 16:08:58 +0000

    lokl

---
+++ Monitoria/Laco de repetição exercicio 7.py
@@ -0,0 +1,24 @@
+import os 
+import time
+
+print("\n== Peso ideal ==")
+
+while True:
+    sexo = str(input("Digite seu sexo: "))
+    if sexo == 'F' or sexo == 'M':
+        break
+    else:
+        print("Sexo inválido, use somente os caracteres 'F' ou 'M'! ")
+
+while True:
+    altura = float(input("Digite sua altura (em cm): "))
+    if altura < 0 or altura > 220:
+        print("Idade incorreta digite um valor em 1cm e 219cm! ")
+
+pesoIdealHomen = (72.7*altura)-58
+pesoIdealMulher = (62.1*altura)-44.7
+
+if sexo == 'F':
+    print(f"Seu peso ideal é: {pesoIdealHomen}")
+else:
+    print(f"Seu peso ideal é: {pesoIdealMulher}")
